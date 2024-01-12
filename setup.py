5f7703fb938660b0da4ca90034357620ce19f887
kiranaradhya20 <kiranaradhya20@gmail.com>
2024-01-13 03:40:56 +0530

    requirements.txt added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'demo1',
+    version= '0.0.0',
+    author= 'kiranaradhya20',
+    author_email= 'kiranaradhya20@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
